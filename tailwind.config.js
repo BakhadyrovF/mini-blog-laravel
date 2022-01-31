d8ad109ec98a3679ad7dcb55864725ae85331b0b
BakhadyrovF <BakhadyrovF@gmail.com>
2022-01-31 13:51:18 +0500

    install laravel-mix/tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
   theme: {
     extend: {},
   },
